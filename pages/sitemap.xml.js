f46e5a3d036198b5c58e9970f670e3b4af15768b
AlexStart2 <fairytailend2003@gmail.com>
2024-02-19 21:30:21 +0200

    sitemap 3

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,59 +1,84 @@
-// pages/sitemap.xml.js
 
-const EXTERNAL_DATA_URL = 'https://turnitgreen-03e15fdc97d7.herokuapp.com/get-article-ids';
+const EXTERNAL_DATA_URL =
+  "https://turnitgreen-03e15fdc97d7.herokuapp.com/get-article-ids";
 
-function generateSiteMap(articleIds) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!-- We manually set the base URL -->
-     <url>
-       <loc>https://www.turnitgreen.eu/</loc>
-     </url>
-     ${articleIds
-       .map((articleId) => {
-         return `
-       <url>
-           <loc>${`https://www.turnitgreen.eu/article/${articleId}`}</loc>
-       </url>
-     `;
-       })
-       .join('')}
-   </urlset>
- `;
-}
+export const revalidate = 3600;
 
-function SiteMap() {
-  // This component doesn't render anything directly.
-  // All the logic is in getServerSideProps.
-}
-
-export async function getServerSideProps({ res }) {
+export async function getServerSideProps() {
   try {
     // Fetch article IDs from the external data URL
     const request = await fetch(EXTERNAL_DATA_URL);
     const { status, data } = await request.json();
 
-    if (status !== 'ok') {
-      throw new Error('Failed to fetch article IDs');
+    if (status !== "ok") {
+      throw new Error("Failed to fetch article IDs");
     }
 
-    // We generate the XML sitemap with the article IDs
-    const sitemap = generateSiteMap(data);
+    const post = data.map((articleId) => ({
+      url: `https://www.turnitgreen.eu/article/${articleId}`,
+      lastModified: new Date().toISOString(),
+      priority: 0.5,
+    }));
 
-    res.setHeader('Content-Type', 'text/xml');
-    // Send the XML sitemap to the browser
-    res.write(sitemap);
-    res.end();
+    const sitemapEntries = [
+      {
+        url: "https://www.turnitgreen.eu",
+        lastModified: new Date().toISOString(),
+        priority: 1,
+      },
+      {
+        url: "https://www.turnitgreen.eu/ESG-knowledge",
+        lastModified: new Date().toISOString(),
+        priority: 1,
+      },
+      {
+        url: "https://www.turnitgreen.eu/ESG-updates",
+        lastModified: new Date().toISOString(),
+        priority: 1,
+      },
+      {
+        url: "https://www.turnitgreen.eu/Search",
+        lastModified: new Date().toISOString(),
+        priority: 1,
+      },
+      {
+        url: "https://www.turnitgreen.eu/about-us",
+        lastModified: new Date().toISOString(),
+        priority: 0.1,
+      },
+      ...post,
+    ];
+
+    return {
+      props: {
+        sitemapEntries,
+      },
+    };
   } catch (error) {
-    console.error('Error generating sitemap:', error.message);
-    res.statusCode = 500;
-    res.end('Error generating sitemap');
+    console.error("Error generating sitemap:", error.message);
+    return {
+      props: {
+        sitemapEntries: [],
+      },
+    };
   }
-
-  // Return an empty object as props (this is required)
-  return {
-    props: {},
-  };
 }
 
-export default SiteMap;
+export default function Sitemap({ sitemapEntries }) {
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${sitemapEntries
+        .map(
+          ({ url, lastModified, priority }) => `
+        <url>
+          <loc>${url}</loc>
+          <lastmod>${lastModified}</lastmod>
+          <priority>${priority}</priority>
+        </url>
+      `
+        )
+        .join("")}
+    </urlset>`;
+
+  return <>{sitemapXml}</>;
+}
